be88281800c518588a3700155bd7891972850430
Cathair Connolly <Connolly-C67@ulster.ac.uk>
2025-09-29 16:55:36 +0100

    Updated program

--- Task1-UserInput/Program.cs
+++ Task1-UserInput/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Please Enter Your Name : ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please Enter Your Age : ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+int result = age + 5;
+
+Console.WriteLine("Hello {0}, you will be {1} in 5 years", name, result);
+
